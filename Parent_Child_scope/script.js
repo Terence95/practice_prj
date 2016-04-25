0707692b64a8d2eb7624e68802df504f7591abf7
terence <wrze@sina.com>
2016-04-25 22:43:28 +0800

    another  example  of scope

---
+++ Parent_Child_scope/script.js
@@ -0,0 +1,29 @@
+var app = angular.module('app', []);
+app.controller('FirstController', function ($scope){
+  // 在scope中创建一个person对象，这个对象只有name这一个属性
+  // $scope.person = {
+  //   name: "Terence"
+  // };
+
+  $scope.person = {greeted: false};
+
+  // ----------------------------- 我是分割线
+  $scope.message = 'World';
+  $scope.counter = '0';
+  $scope.add = function(amount){
+    $scope.counter += amount;
+  }
+
+  $scope.substract = function(amount){
+    $scope.counter -= amount;
+  }
+
+
+});
+
+app.controller('FirstChildController', function($scope){
+  $scope.sayHello = function() {
+    $scope.person.name = "tERENCE";
+    $scope.person.greeted = true;
+  }
+});
