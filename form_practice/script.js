8de28c94b266dac783e74dc9fac4bded7d434196
terence <wrze@sina.com>
2016-04-28 11:55:20 +0800

    v3

--- form_practice/script.js
+++ form_practice/script.js
@@ -1,50 +1,25 @@
-var app = angular.module('myApp', ['emailParser']);
-var epaser = angular.module('emailParser', []);
-app.controller("myController",
-  function($scope, $parse) {
-    $scope.$watch('expr', function(newVal, oldVal, scope) {
-      if (newVal !== oldVal) {
-        // 用表达式
-        var parseFun = $parse(newVal);
-        // 获取解析之后的表达式的值
-        $scope.parseValue = parseFun(scope);
-      }
-    });
-  });
-
-app.controller("secController", function ($scope, $interpolate) {
-  // 同时拥有访问 $scope 和 $interpolate 的权限
-  $scope.$watch("emailBody", function(body) {
-    // $scope.to = 'ari@fullstack.io';
-    // $scope.emailBody = 'Hello {{ to }},\n\nMy name is Ari too!';
-    if (body) {
-      var templete = $interpolate(body);
-      $scope.previewText = templete({to: $scope.to});
-    }
-  });
+var app = angular.module("myApp", []);
+app.directive('ensureUnique', function($http) {
+    return {
+        require: 'ngModel',
+        link: function(scop, attrs, c) {
+            scope.$watch(attrs.ngModel, function(n) {
+                if (!n) {
+                    return;
+                }
+                $http({
+                    method: 'POST',
+                    url: '/api/check/' + attrs.ensureUnique,
+                    data: {
+                        field: attrs.ensureUnique,
+                        value: scope.ngModel
+                    }
+                }).success(function(data) {
+                    c.$setValidity('unique', data.isUnique);
+                }).error(function(data) {
+                    c.$setValidity('unique', false);
+                });
+            });
+        }
+    };
 });
-
-app.controller("thirdController", ['$scope', 'EmailParser', function($scope, EmailParser){
-  // 设置监听
-  $scope.$watch("emailBody2", function(body){
-    if (body) {
-      $scope.previewText = EmailParser.parse(body, {
-        to: $scope.to
-      });
-    }
-  });
-}]);
-
-epaser.config(['$interpolateProvider', function($interpolateProvider){
-  $interpolateProvider.startSymbol('__');
-  $interpolateProvider.endSymbol('__');
-}])
-.factory('EmailParser', ['$interpolate', function($interpolate) {
-  // 处理解析的服务
-  return{
-    parse: function(text, context) {
-      var templete = $interpolate(text);
-      return templete(context);
-    }
-  };
-}]);
