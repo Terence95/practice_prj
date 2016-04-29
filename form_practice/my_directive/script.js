6889c78f3032b500919ed78a2e1d7934b572df22
terence <wrze@sina.com>
2016-04-29 22:28:01 +0800

    chapter9.2

--- form_practice/my_directive/script.js
+++ form_practice/my_directive/script.js
@@ -1,5 +1,10 @@
 var app = angular.module("myApp", []);
-
+app.run(function($rootScope, $timeout) {
+    $timeout(function() {
+        $rootScope.myHref = "https://www.zhihu.com/";
+        $rootScope.imgSrc = "../img/testimg.jpg";
+    }, 2000);
+});
 app.directive('myDirective', function() {
     return {
         scope: {
@@ -22,6 +27,10 @@ app.controller('ParentController', function($scope) {
     // 使用.controller访问 ‘ng-controller’ 内部属性
     // 在DOM忽略的$scope中，根据当前控制器进行推断
     $scope.parentProperty = "parent scope";
+    $scope.someBareValue = "hello computer";
+    $scope.parentAction = function(){
+      $scope.someBareValue = "hello human, from parent";
+    };
 });
 
 app.controller('ChildController', function($scope) {
@@ -32,4 +41,19 @@ app.controller('ChildController', function($scope) {
         $scope.rootProperty + ' and ' +
         $scope.parentProperty + ' and ' +
         $scope.childProperty;
+    $scope.childAction = function () {
+      // 在childctrl中设置 {{}}
+      $scope.someBareValue = 'hello homan,from child';
+    };
+});
+app.controller('SomeController', function($scope) {
+    // 创建模型
+    $scope.someModel = {
+        // 添加属性，
+        someProperty: 'Hello Computer'
+    };
+    // 设置$scope自身的操作
+    $scope.someAction = function() {
+        $scope.someModel.someProperty = 'hello human';
+    };
 });
