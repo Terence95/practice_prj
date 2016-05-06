839987d1f80bae88742f3e95ac18a78ee69b7c5f
terence <wrze@sina.com>
2016-05-06 21:57:05 +0800

    add edit

--- onePiece/script.js
+++ onePiece/script.js
@@ -4,37 +4,40 @@ onePiece.config(['$routeProvider', function($routeProvider) {
     $routeProvider
         .when('/', {
             templateUrl: 'view.html'
+        }).when('/edit', {
+            templateUrl: 'edit.html'
+        }).otherwise({
+            redirectTo: '/'
         });
 }]);
 
 onePiece.controller('OnePieceCtrl', function($scope) {
     $scope.friends = [{
-            name: 'Monkey D Luffy',
-            reward: 400000000
-        }, {
-            name: 'Brook',
-            reward: 83000000
-        }, {
-            name: 'Roronoa Zoro',
-            reward: 320000000
-        }, {
-            name: 'Nami',
-            reward: 66000000
-        }, {
-            name: 'Usopp',
-            reward: 200000000
-        }, {
-            name: 'Sanji',
-            reward: 177000000
-        }, {
-            name: 'Chopper',
-            reward: 100
-        }, {
-            name: 'Nico Robin',
-            reward: 130000000
-        }, {
-            name: 'Franky',
-            reward: 94000000
-        }
-    ];
+        name: 'Monkey D Luffy',
+        reward: 400000000
+    }, {
+        name: 'Brook',
+        reward: 83000000
+    }, {
+        name: 'Roronoa Zoro',
+        reward: 320000000
+    }, {
+        name: 'Nami',
+        reward: 66000000
+    }, {
+        name: 'Usopp',
+        reward: 200000000
+    }, {
+        name: 'Sanji',
+        reward: 177000000
+    }, {
+        name: 'Chopper',
+        reward: 100
+    }, {
+        name: 'Nico Robin',
+        reward: 130000000
+    }, {
+        name: 'Franky',
+        reward: 94000000
+    }];
 });
