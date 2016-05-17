fdb9158948e8aa037ac1a41e730033f41dab880d
terence <wrze@sina.com>
2016-05-17 11:21:34 +0800

    加了另一个例子

--- practice_service/script.js
+++ practice_service/script.js
@@ -17,43 +17,30 @@
 //   this.value = 'v';
 // });
 
-
-// 一个实例
 var app = angular.module('myApp', []);
-
-app.factory('githubService', function($http) {
-    // serviceInstance可以在函数定义中访问$http服务
-    var githubUrl = 'https://api.github.com';
-
-    var runUserRequest = function(username, path) {
-      // 从使用jsonp调用git api 的$http 服务中返回promise
-      return $http({
-        methd: 'JSONP',
-        url: githubUrl + '/users' +
-              username + '/' +
-              path + '?callback = JSON_CALLBACK'
-      });
+app.controller('defaultCtrl', function($scope) {
+    $scope.data = {
+      cities: ['London', 'New York', 'Paris'],
+      totalClicks: 0
     };
 
-    // 返回一个带有events函数的服务对象
-    return {
-      events: function(username) {
-        return runUserRequest(username, 'events');
-      },
-      setUsername: function(username) {
-        githubUsername = username;
-      }
-    };
+    $scope.$watch('data.totalClicks', function(newVal) {
+      console.log("Total click count: " + newVal);
+    });
 });
 
-app.controller('ServiceController', function($scope, githubService) {
-      // 我们可以调用对象的事件函数
-      // $scope.events = githubService.events('auser');
-      $scope.$watch('username', function (newUsername) {
-        //
-        githubService.events(newUsername)
-        .success(function(data, status, headers){
-            $scope.events = data.data;
+app.directive('triButton', function() {
+    return {
+      scope: {counter: "=counter"},
+      link: function(scope,element,attrs) {
+        element.on("click", function(event) {
+          // 输出那一个城市被点击
+          // event.target应该是得到了被点击的那个按钮元素
+          console.log("Button click: " + event.target.innerText);
+          scope.$apply(function(){
+            scope.counter++;
+          });
         });
-      });
+      }
+    };
 });
