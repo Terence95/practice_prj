41272d4ad7c25f176a3c4f1915aceeb013b59370
terence <wrze@sina.com>
2016-05-17 23:17:19 +0800

    添加了一个关于服务的例子

--- practice_service/script.js
+++ practice_service/script.js
@@ -60,3 +60,40 @@ app.controller('LoadDataCtrl', ['$scope', '$http', function($scope, $http) {
         console.log("error");
     });
 }]);
+
+
+// 创建一个自己的服务
+app.factory('userListService', ['$http',
+    function($http) {
+        var doRequest = function(username, path) {
+            return $http({
+                method: 'GET',
+                url: 'package.json'
+            });
+        };
+        return {
+            userList: function(username) {
+                return doRequest(username, 'userList');
+            }
+        };
+    }
+]);
+
+myServiceApp.controller('ServiceController', ['$scope', '$timeout', 'userListService',
+    function($scope, $timeout, userListService) {
+        var timeout;
+        $scope.$watch('username', function(newUsername) {
+            if (newUsername) {
+                if (timeout) {
+                    $timeout.cancel(timeout);
+                }
+                timeout = $timeout(function() {
+                    userListService.userList(newUsername)
+                        .success(function(data, status) {
+                            $scope.users = data;
+                        });
+                }, 350);
+            }
+        });
+    }
+]);
