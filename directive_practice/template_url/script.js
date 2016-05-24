a5eb3885ef74c37ded798b776ffd60fb1e756a0e
terence <wrze@sina.com>
2016-05-24 10:53:26 +0800

    an example use angular-ui

--- directive_practice/template_url/script.js
+++ directive_practice/template_url/script.js
@@ -153,3 +153,30 @@ appModule.directive("myDirective", function() {
         template:'<div><a href="https://www.baidu.com/">click me to go to baidu</a></div>'
     };
 });
+
+
+appModule.directive('expander', function () {
+    return {
+      restrict: 'AE',
+      replace:true,
+      transclude: true,
+      scope: {
+        title: '=expanderTitle'
+      },
+      template: '<div>'
+              + '<div class="title" ng-click="toggle()">{{title}}</div>'
+              + '<div class="body" ng-show="showMe" ng-transclude></div>'
+              + '</div>',
+      link: function (scope, element, attrs) {
+          scope.showMe = false;
+          scope.toggle = function () {
+              scope.showMe = !scope.showMe;
+          };
+      }
+    };
+});
+
+appModule.controller('expanderCtrl', function ($scope) {
+      $scope.title = '点击展开';
+      $scope.text = '内部内容';
+});
