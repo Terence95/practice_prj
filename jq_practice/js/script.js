96483d3d2e411806f4c9e52422b53bea7a736e88
terence <wrze@sina.com>
2016-06-20 21:58:51 +0800

    practice

--- jq_practice/js/script.js
+++ jq_practice/js/script.js
@@ -59,4 +59,37 @@ $(document).ready(function() {
         div.animate({height:'100px', opacity:'0.4'}, "slow");
         div.animate({width:'100px', opacity:'0.8'}, "slow");
     });
+
+    // first选择器
+    // $(".part2 div:first").css("background-color", "red");
+    // even 偶数的选择器，index从0开始
+    // $(".part2 div:even").css("background-color", "red");
+    // odd 奇数的选择器，index从0开始
+    // $(".part2 div:odd").css("background-color", "red");
+
+    //
+    // $(".part2 div:eq(2)").css("background-color", "red");
+
+    // 选范围的选择器 gt greater than
+    // $(".part2 div:gt(0)").css("background-color", "red");
+
+    // 小于 less than
+
+    // 一个枚举的例子，又要2，又要1
+    $(".part2 div:eq(2), .part2 div:eq(1)").css("background-color", "red");
+
+
+    // 内容选择器 找到包含“放假”的p标签
+    $("p:contains(放假)").css("color", "red");
+
+    // 空选择器 不包含子元素或文本的元素 <p></p> 这东西还不能有回车键
+    $("p[name='msg']:empty").html("hello world");
+
+
+
+
+
+
+
+
 });
